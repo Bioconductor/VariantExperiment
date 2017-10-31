110bcc5aa4b9719bb707b2a06b7e22d4f997e26f
Qian Liu <qliu7@buffalo.edu>
2017-10-31 16:25:54 -0400

    Defined GDSArraySeed with accessors, show method and constructor from gds file.

--- R/GDSArray-class.R
+++ R/GDSArray-class.R
@@ -1,21 +1,49 @@
+#' @import SNPRelate
+#' @import gdsfmt
+#' @importFrom tools file_path_as_absolute
+#' @importFrom S4Vectors isSingleString isSingleStringOrNA
+#' 
+
+## library(gdsfmt)
+## library(SNPRelate)
+## library(S4Vectors)
+## library(tools)
+
 ### =========================================================================
 ### GDSArray objects
 ### -------------------------------------------------------------------------
 
-
 setClass("GDSArraySeed",
-    contains = "Array", ## ?? or "gds.class"
+    contains = "array", ## ?? or "gds.class"
     slots = c(
         file="character",   # Absolute path to the gds file so the object
                             # doesn't break when the user changes the working
                             # directory (e.g. with setwd()).
         dim = "integer",
-        dimnames = "list"
-        ## first_val="ANY"  # First value in the dataset. Needed for GDSArraySeed???? 
+        dimnames = "list",
+        first_val="ANY"  # First value in the dataset. Needed for GDSArraySeed???? 
     )
 )
 
+###
+## accessors for GDSArraySeed object
+###
 setMethod("dim", "GDSArraySeed", function(x) x@dim)
+setMethod("dimnames", "GDSArraySeed", function(x) x@dimnames)
+
+###
+## show method for GDSArraySeed object
+###
+setMethod(
+    "show", "GDSArraySeed",
+    function(object){
+        cat("GDSArraySeed\n")
+        cat("gds file:", object@file, "\n")
+        cat("The total number of samples: ", unname(dim(object)["sample.id"]), "\n")
+        cat("The total number of snps: ", unname(dim(object)["snp.id"]), "\n")
+        cat("The first value of assay:", object@first_val, "\n")
+    }
+)
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -43,18 +71,17 @@ setMethod("subset_seed_as_array", "GDSArraySeed",
 ### GDSArraySeed constructor
 ###
 
-## library(SeqArray)
-## file <- seqExampleFileName("gds")
-## file <- seqOpen(fn)
-#' @import SeqArray
+## ## library(SeqArray)
+## ## file <- seqExampleFileName("gds")
+## ## library(SNPRelate)
+## ## file <- seqOpen(fn)
 
-.get_gdsdata_dim <- function(file)
-{
-    f <- seqOpen(file)
-    sample.id <- seqGetData(f, "sample.id")
-    variant.id <- seqGetData(f, "variant.id")
-    seqClose(f)
-    dim <- c(length(sample.id), length(variant.id))
+## file <- system.file("extdata", "hapmap_geno.gds", package = "SNPRelate")
+## snpgdsSummary(file)
+## f <- snpgdsOpen(file)
+
+.get_gdsdata_dim <- function(file){
+    dim <- sapply(snpgdsSummary(file, show=FALSE), length)
     if (!is.integer(dim)) {
         if (any(dim > .Machine$integer.max)) {
             dim_in1string <- paste0(dim, collapse=" x ")
@@ -68,90 +95,88 @@ setMethod("subset_seed_as_array", "GDSArraySeed",
     dim
 }
 
-### Will fail if the dataset is empty (i.e. if at least one of its dimensions
-### is 0).
-## .read_gdsdata_first_val <- function(file, name, ndim)
-.read_gdsdata_first_val <- function(file)
-{
-    f <- seqOpen(file)
-    sample.id <- seqGetData(f, "sample.id")
-    variant.id <- seqGetData(f, "variant.id")
-    suppressMessages(
-        seqSetFilter(f, sample.id=sample.id[1], variant.id=variant.id[1])
-        )   ## doesn't work. 
-    first_val <- paste(seqGetData(f, "genotype")[, 1, ], collapse="/")
-    suppressMessages(seqResetFilter(f))
-    seqClose(f)
+.get_gdsdata_dimnames <- function(file){
+    dimnames <- snpgdsSummary(file, show=FALSE)
+    if (!is.list(dimnames)) {
+        stop(wmsg("The dimnames of GDS dataset '", file, "' should be a list!"))
+    }
+    dimnames
+}
+## if dimnames is shorter than the corresponding dimensions, use NULL to extend.??
+
+.read_gdsdata_first_val <- function(file){
+    f <- snpgdsOpen(file)
+    first_val <- read.gdsn(index.gdsn(f, "genotype"), start=c(1,1), count=c(1,1))
+    snpgdsClose(f)
     first_val
-    ## index <- rep.int(list(1L), ndim)
-    ## ans <- h5read2(file, name, index)
-    ## stopifnot(length(ans) == 1L)  # sanity check
-    ## ans[[1L]]  # drop any attribute
 }
 
+## vcf.fn <- system.file("extdata", "sequence.vcf", package="SNPRelate")
+## snpgdsVCF2GDS(vcf.fn, "test2.gds", method="biallelic.only", snpfirstdim=TRUE)
+## snpgdsSummary("test2.gds")  ## 2snp X 3samples
+## f2 <- snpgdsOpen("test2.gds")
+## snpgdsClose(f2)
+
 ### Return a GDSArraySeed object with NO dimnames!
-### FIXME: Investigate the possiblity to store the dimnames in the HDF5 file
+### FIXME: Investigate the possiblity to store the dimnames in the GDS file
 ### and make dimnames() on the object returned by GDSArraySeed() bring them
 ### back.
-GDSArraySeed <- function(file, name, type=NA)
-{
+GDSArraySeed <- function(file, type=NA){
     if (!isSingleString(file))
         stop(wmsg("'file' must be a single string specifying the path to ",
-                  "the HDF5 file where the dataset is located"))
+                  "the gds file where the dataset is located."))
     file <- file_path_as_absolute(file)
-    if (!isSingleString(name))
-        stop(wmsg("'name' must be a single string specifying the name ",
-                  "of the dataset in the HDF5 file"))
-    if (name == "")
-        stop(wmsg("'name' cannot be the empty string"))
     if (!isSingleStringOrNA(type))
         stop("'type' must be a single string or NA")
-    dim <- .get_gdsdata_dim(file, name)
-    if (any(dim == 0L)) {
-        if (is.na(type))
-            stop(wmsg("This HDF5 dataset is empty! Don't know how to ",
-                      "determine the type of an empty HDF5 dataset at the ",
-                      "moment. Please use the 'type' argument to help me ",
-                      "(see '?GDSArray' for more information)."))
-        first_val <- match.fun(type)(1)  # fake value
-        if (!is.atomic(first_val))
-            stop(wmsg("invalid type: ", type))
-    } else {
-        first_val <- .read_gdsdata_first_val(file, name, length(dim))
-        detected_type <- typeof(first_val)
-        if (!(is.na(type) || type == detected_type))
-            warning(wmsg("The type specified via the 'type' argument (",
-                         type, ") doesn't match the type of this HDF5 ",
-                         "dataset (", detected_type, "). Ignoring the ",
-                         "former."))
-    }
+    dim <- .get_gdsdata_dim(file)
+    dimnames <- .get_gdsdata_dimnames(file)
+    first_val <- .read_gdsdata_first_val(file)
+    ## if (any(dim == 0L)) {
+    ## if (is.na(type))
+    ##     stop(wmsg("This gds dataset is empty! Don't know how to ",
+    ##               "determine the type of an empty gds dataset at the ",
+    ##               "moment. Please use the 'type' argument to help me ",
+    ##               "(see '?GDSArray' for more information)."))
+    
+    ## first_val <- match.fun(type)(1)  # fake value
+    ## if (!is.atomic(first_val))
+    ##         stop(wmsg("invalid type: ", type))
+    ## } else {
+    ## first_val <- .read_gdsdata_first_val(file, length(dim))
+    ## detected_type <- typeof(first_val)
+    ## if (!(is.na(type) || type == detected_type))
+    ##     warning(wmsg("The type specified via the 'type' argument (",
+    ##                  type, ") doesn't match the type of this GDS ",
+    ##                  "dataset (", detected_type, "). Ignoring the ",
+    ##                  "former."))
+    ## }
     new2("GDSArraySeed", file=file,
-                          name=name,
-                          dim=dim,
-                          first_val=first_val)
+         dim=dim,
+         dimnames = dimnames,
+         first_val=first_val)
 }
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### GDSArray and HDF5Matrix objects
+### GDSArray and GDSMatrix objects
 ###
 ### We define these classes only for cosmetic reasons i.e. to hide the
 ### DelayedArray and DelayedMatrix classes from the user. The user will see
-### and manipulate GDSArray and HDF5Matrix objects instead of DelayedArray
+### and manipulate GDSArray and GDSMatrix objects instead of DelayedArray
 ### and DelayedMatrix objects.
 ###
 
 setClass("GDSArray", contains="DelayedArray")
 
-setClass("HDF5Matrix", contains=c("DelayedMatrix", "GDSArray"))
+setClass("GDSMatrix", contains=c("DelayedMatrix", "GDSArray"))
 
-### Automatic coercion method from GDSArray to HDF5Matrix silently returns
+### Automatic coercion method from GDSArray to GDSMatrix silently returns
 ### a broken object (unfortunately these dummy automatic coercion methods don't
 ### bother to validate the object they return). So we overwrite it.
-setAs("GDSArray", "HDF5Matrix", function(from) new("HDF5Matrix", from))
+setAs("GDSArray", "GDSMatrix", function(from) new("GDSMatrix", from))
 
 ### For internal use only.
-setMethod("matrixClass", "GDSArray", function(x) "HDF5Matrix")
+setMethod("matrixClass", "GDSArray", function(x) "GDSMatrix")
 
 .validate_GDSArray <- function(x)
 {
@@ -164,8 +189,8 @@ setMethod("matrixClass", "GDSArray", function(x) "HDF5Matrix")
 
 setValidity2("GDSArray", .validate_GDSArray)
 
-setAs("ANY", "HDF5Matrix",
-    function(from) as(as(from, "GDSArray"), "HDF5Matrix")
+setAs("ANY", "GDSMatrix",
+    function(from) as(as(from, "GDSArray"), "GDSMatrix")
 )
 
 

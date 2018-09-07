2578c4f3482655e7d8419c94d09f62c6765cfabb
Qian Liu <qliu7@buffalo.edu>
2018-09-07 11:09:44 -0400

    cleanup commented code. bump version.

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -6,7 +6,7 @@ setGeneric("seqAlleleFreq",
            function(gdsfile, ref.allele=0L, .progress=FALSE,
                     parallel=seqGetParallel())
                standardGeneric("seqAlleleFreq"),
-           useAsDefault=seqAlleleFreq,
+           useAsDefault=SeqArray::seqAlleleFreq,
            signature="gdsfile")
 setGeneric("seqAlleleCount",
            function(gdsfile, ref.allele=0L, .progress=FALSE,
@@ -23,7 +23,7 @@ setGeneric("seqMissing",
 setGeneric("seqNumAllele",
            function(gdsfile)
                standardGeneric("seqNumAllele"),
-           useAsDefault=seqNumAllele,
+           useAsDefault=SeqArrayseqNumAllele,
            signature="gdsfile")
 ### note:
 ## seqNumAllele@generic
@@ -33,65 +33,3 @@ setGeneric("seqNumAllele",
 ## document()
 ## Creating a generic function for ‘seqNumAllele’ from package ‘SeqArray’ in package ‘VariantExperiment’
 
-## ## SeqVarTools: implemented: hwe, inbreedCoeff, pca, titv, refDosage, altDosage, countSingletons, homozygosity, heterozygosity, meanBySample, missingGenotypeRate, isSNV, isVariant
-## setGeneric("hwe",
-##            function(gdsobj, ...) standardGeneric("hwe"),
-##            useAsDefault=SeqVarTools::hwe,
-##            signature="gdsobj")
-## setGeneric("inbreedCoeff",
-##            function(gdsobj, ...) standardGeneric("inbreedCoeff"),
-##            useAsDefault=SeqVarTools::inbreedCoeff,
-##            signature="gdsobj")
-## setGeneric("pca",
-##            function(gdsobj, ...) standardGeneric("pca"),
-##            useAsDefault=SeqVarTools::pca,
-##            signature="gdsobj")
-## setGeneric("titv",
-##            function(gdsobj, ...) standardGeneric("titv"),
-##            useAsDefault=SeqVarTools::titv,
-##            signature="gdsobj")
-## setGeneric("refDosage",
-##            function(gdsobj, ...) standardGeneric("refDosage"),
-##            useAsDefault=SeqVarTools::refDosage,
-##            signature="gdsobj")
-## setGeneric("altDosage",
-##            function(gdsobj, ...) standardGeneric("altDosage"),
-##            useAsDefault=SeqVarTools::altDosage,
-##            signature="gdsobj")
-## setGeneric("countSingletons",
-##            function(gdsobj, ...) standardGeneric("countSingletons"),
-##            useAsDefault=SeqVarTools::countSingletons,
-##            signature="gdsobj")
-## setGeneric("homozygosity",
-##            function(gdsobj, ...) standardGeneric("homozygosity"),
-##            useAsDefault=SeqVarTools::homozygosity,
-##            signature="gdsobj")
-## setGeneric("heterozygosity",
-##            function(gdsobj, ...) standardGeneric("heterozygosity"),
-##            useAsDefault=SeqVarTools::heterozygosity,
-##            signature="gdsobj")
-## setGeneric("meanBySample",
-##            function(gdsobj, ...) standardGeneric("meanBySample"),
-##            useAsDefault=SeqVarTools::meanBySample,
-##            signature="gdsobj")
-## setGeneric("missingGenotypeRate",
-##            function(gdsobj, ...) standardGeneric("missingGenotypeRate"),
-##            useAsDefault=SeqVarTools::missingGenotypeRate,
-##            signature="gdsobj")
-## setGeneric("isSNV",
-##            function(gdsobj, ...) standardGeneric("isSNV"),
-##            useAsDefault=SeqVarTools::isSNV,
-##            signature="gdsobj")
-## setGeneric("isVariant",
-##            function(gdsobj, ...) standardGeneric("isVariant"),
-##            useAsDefault=SeqVarTools::isVariant,
-##            signature="gdsobj")
-
-## 1 case: setGeneric:
-## @importMethodsFrom seqArray
-## @import SeqArray except=rowRanges
-## .rowRanges <- function(x, ...){
-##     seqArray::rowRanges(x)
-## }
-## setMethod("rowRanges", "VariantExperiment", .rowRanges)
-## #' @export rowRanges
